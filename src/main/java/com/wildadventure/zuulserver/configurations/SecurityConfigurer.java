f6963b832b27e2f6bfaf43008cad59342afd523f
Titouan Raimbault <t_raimbault@yahoo.fr>
2020-09-17 15:15:00 +0200

    #WA-49 : configure CORS Policy in spring security configuration

---
+++ src/main/java/com/wildadventure/zuulserver/configurations/SecurityConfigurer.java
@@ -0,0 +1,40 @@
+package com.wildadventure.zuulserver.configurations;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
+	
+	private final Logger logger = LoggerFactory.getLogger(SecurityConfigurer.class);
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.cors();
+		http.csrf().disable();
+	}
+
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("accept", "authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
